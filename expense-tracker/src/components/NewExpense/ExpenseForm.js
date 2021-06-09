7980a6e0724b653d9f668e47fd416ebad3303587
Ryan Johnson <ryanleejwebdev@gmail.com>
2021-06-09 13:51:48 -0500

    Batch up form data into an object on submission and use 2-way data binding

--- expense-tracker/src/components/NewExpense/ExpenseForm.js
+++ expense-tracker/src/components/NewExpense/ExpenseForm.js
@@ -9,6 +9,8 @@ const ExpenseForm = () => {
   const [enteredTitle, setEnteredTitle] = useState('')
   const [enteredAmount, setEnteredAmount] = useState('')
   const [enteredDate, setEnteredDate] = useState('')
+  //! two way binding is very useful when you're dealing w/ forms
+  // - just give them a value that uses a state variable
 
   //! ALT: multiple states in 1, by passing an obj as a value
   // but now whenever you update a piece of the state, you have to update all 3
@@ -44,20 +46,37 @@ const ExpenseForm = () => {
     setEnteredDate(e.target.value)
   }
 
+  const submitHandler = (e) => {
+    e.preventDefault()
+
+    // batch up the form data into a single object
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate)
+    }
+
+    console.log(expenseData)
+    // clear form values after submit
+    setEnteredTitle('')
+    setEnteredAmount('')
+    setEnteredDate('')
+  }
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label htmlFor="">Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input type="text" onChange={titleChangeHandler} value={enteredTitle} />
         </div>
         <div className="new-expense__control">
           <label htmlFor="">Amount</label>
-          <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} />
+          <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} value={enteredAmount} />
         </div>
         <div className="new-expense__control">
           <label htmlFor="">Date</label>
-          <input type="date" min="2020-01-01" max="2022-12-31" onChange={dateChangeHandler} />
+          <input type="date" min="2020-01-01" max="2022-12-31" onChange={dateChangeHandler} value={enteredDate} />
         </div>
       </div>
       <div className="new-expense__actions">
