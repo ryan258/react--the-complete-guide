1afd95e91bf59d9f953457dc4cbd47b71a1ede8c
Ryan Johnson <ryanleejwebdev@gmail.com>
2021-06-09 13:31:08 -0500

    Add state to <ExpenseForm /> and noted an alternative way of handling state

--- expense-tracker/src/components/NewExpense/ExpenseForm.js
+++ expense-tracker/src/components/NewExpense/ExpenseForm.js
@@ -1,22 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import './ExpenseForm.css'
 
 const ExpenseForm = () => {
+  // state will detach the variable from the lifecycle, thus survives all that
+  // and when it updates the component rerenders
+  //! you can and probably will have multiple states per component
+  const [enteredTitle, setEnteredTitle] = useState('')
+  const [enteredAmount, setEnteredAmount] = useState('')
+  const [enteredDate, setEnteredDate] = useState('')
+
+  //! ALT: multiple states in 1, by passing an obj as a value
+  // but now whenever you update a piece of the state, you have to update all 3
+  // const [userInput, setUserInput] = useState({
+  //   enteredTitle: '',
+  //   enteredAmount: '',
+  //   enteredDate: ''
+  // })
+
+  //! fun fact: whenever you read the value of an input value, it'll be a string, which is why we're initializing all out states w/ an empty string
+  const titleChangeHandler = (e) => {
+    //! ALT:
+    // then to update you would pass in a new obj - but it will replace, not merge the new one
+    // - this would dump the other 2 states
+    // setUserInput({
+    // so you need to copy in the other 2 values to keep them around
+    //...userInput,
+    //enteredTitle: e.target.value,
+    // })
+    // but ^^^ updating state like this isn't best practice, bc whenever you depend on the previous state, so call it like this vvv
+    // setUserInput((prevState) => {
+    //   return { ...prevState, enteredTitle: e.target.value }
+    // })
+    // console.log('title changed!', e)
+    setEnteredTitle(e.target.value)
+  }
+
+  const amountChangeHandler = (e) => {
+    setEnteredAmount(e.target.value)
+  }
+
+  const dateChangeHandler = (e) => {
+    setEnteredDate(e.target.value)
+  }
+
   return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label htmlFor="">Title</label>
-          <input type="text" />
+          <input type="text" onChange={titleChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label htmlFor="">Amount</label>
-          <input type="number" min="0.01" step="0.01" />
+          <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label htmlFor="">Date</label>
-          <input type="date" min="2020-01-01" max="2022-12-31" />
+          <input type="date" min="2020-01-01" max="2022-12-31" onChange={dateChangeHandler} />
         </div>
       </div>
       <div className="new-expense__actions">
