9962bf90180234af2a2b79e51ef6a3892609f6db
Ryan Johnson <ryanleejwebdev@gmail.com>
2021-06-14 09:53:00 -0500

    Add <Chart /> and wire it up with Expenses

---
+++ expense-tracker/src/components/Chart/Chart.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+import './Chart.css'
+import ChartBar from './ChartBar'
+
+// receive data points as props
+const Chart = ({ dataPoints }) => {
+  const dataPointValues = dataPoints.map((dataPoint) => dataPoint.value)
+  const totalMaximum = Math.max(...dataPointValues)
+
+  return (
+    <div className="chart">
+      {dataPoints.map((dataPoint) => (
+        <ChartBar //
+          key={dataPoint.label}
+          value={dataPoint.value}
+          maxValue={totalMaximum}
+          label={dataPoint.label}
+        />
+      ))}
+    </div>
+  )
+}
+
+export default Chart
--- expense-tracker/src/components/Expenses/Expenses.js
+++ expense-tracker/src/components/Expenses/Expenses.js
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import Card from '../UI/Card'
 import ExpensesFilter from './ExpensesFilter'
 import ExpensesList from './ExpensesList'
+import ExpensesChart from './ExpensesChart'
 
 import './Expenses.css'
 
@@ -21,6 +22,7 @@ const Expenses = ({ items }) => {
       <Card className="expenses">
         {/* Render the list dynamically */}
         <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+        <ExpensesChart expenses={filteredExpenses} />
         <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
